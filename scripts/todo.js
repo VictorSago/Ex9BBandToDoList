9255521516e0880ca6f035e7a69616630a0a545d
Victor Sago <VictorSago01@gmail.com>
2021-03-02 11:37:30 +0100

    Tasks get added

--- scripts/todo.js
+++ scripts/todo.js
@@ -6,4 +6,11 @@ let tdList = document.querySelector('#td-list');
 function AddTask() {
     let taskEntry = document.querySelector('#td-task').value;
     // console.log(taskEntry);
+    let tdItem = document.createElement("li");
+    var tdText = document.createTextNode(taskEntry);
+    tdItem.appendChild(tdText);
+    
+    tdList.appendChild(tdItem);
+
+    document.querySelector('#td-task').value = '';
 }
