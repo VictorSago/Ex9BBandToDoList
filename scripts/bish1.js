35a7f51aa50f734d32dbec6b0ae857c424ec3627
Victor Sago <VictorSago01@gmail.com>
2021-03-02 01:57:05 +0100

    Init. Bish1 complete. Bish2 form complete.

---
+++ scripts/bish1.js
@@ -0,0 +1,23 @@
+let output_area = document.querySelector('#output-area');
+log_message = BishBosh1();
+
+console.log(log_message);
+
+output_area.innerText = log_message;
+
+function BishBosh1() {
+    let outputText = '';
+    for (let i = 1; i <= 100; i++) {
+        if (i % 12 === 0) {
+            outputText += "BishBosh";
+        } else if (i % 3 === 0) {
+            outputText += "Bish";
+        } else if (i % 4 === 0) {
+            outputText += "Bosh";
+        } else {
+            outputText += i;
+        }
+        outputText += ', ';
+    }
+    return outputText.slice(0, -2);
+}
